c4bcf54dfd18d7724e683c2709318521c35ec70a
aakarshit <aakarshit.jaswal@infostride.com>
2023-10-20 23:49:37 +0530

    WIP: Restart

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -16,17 +16,59 @@ struct OnboardingView: View {
     
     //MARK: BODY
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding").font(.largeTitle)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
             
-            Button(action: {
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
-            }
+            VStack(spacing: 20) {
+                
+                //MARK: HEADER
+                
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not how much we give but
+                    how much love we put into giving.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                } //: HEADER
+                
+                //MARK: CENTER
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    } //: ZSTACK
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } //: CENTER
+                
+                Spacer()
+                
+                //MARK: FOOTER
+                
 
-        }
-    } //:VSTACK
+                
+            } //:VSTACK
+        } //: ZSTACK
+    }
 }
 
 struct OnboardingView_Previews: PreviewProvider {
