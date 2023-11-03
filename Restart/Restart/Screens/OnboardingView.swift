f65fce53c0127cf46a6d806ae415923f68c25f80
Aakarshit Jaswal <70897906+aakarshitjaswal@users.noreply.github.com>
2023-11-03 16:22:38 +0530

    Restart: Add Gesture to Image View

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -16,6 +16,7 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = .zero
     
     
     //MARK: BODY
@@ -62,6 +63,13 @@ struct OnboardingView: View {
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.5), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y: 0)
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    imageOffset = gesture.translation
+                                })
+                        )
                 } //: CENTER
                 
                 Spacer()
