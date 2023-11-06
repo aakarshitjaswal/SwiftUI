768d133ca5784a4cf1e0c43d5edc2612c829e1c6
Aakarshit Jaswal <70897906+aakarshitjaswal@users.noreply.github.com>
2023-11-06 16:20:28 +0530

    Recreate Pinch: Nested Repo Fixed

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Aakarshit Jaswal on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
