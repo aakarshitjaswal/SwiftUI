d084f54f17e56b8f69264354bba3491d0c460d41
Aakarshit Jaswal <70897906+aakarshitjaswal@users.noreply.github.com>
2023-11-07 16:49:11 +0530

    Pinch: Add Double Tap Gestrue & more

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTENT
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+
+        NavigationView {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2),radius: 12,x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                            
+                        }
+                    })
+            } //: ZSTACK
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                isAnimating = true
+            })
+            
+        } //: NAVIGATION
+
     }
 }
 
