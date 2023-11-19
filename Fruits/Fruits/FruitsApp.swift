09ffd13952b50e719eb2d410ccfbdcfbff09b7c3
aakarshitjaswal <aakarshitjaswal@gmail.com>
2023-11-19 22:34:39 +0530

    Fruits: Create Project

---
+++ Fruits/Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by aakarshit on 19/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
