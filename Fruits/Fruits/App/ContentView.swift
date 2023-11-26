0b2e98eb77497162c5a9f8186ca0f1674780fa7d
Aakarshit Jaswal <70897906+aakarshitjaswal@users.noreply.github.com>
2023-11-26 19:16:53 +0530

    FRUITS: CREATE: Fruits Row List

--- Fruits/Fruits/App/ContentView.swift
+++ Fruits/Fruits/App/ContentView.swift
@@ -8,17 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: PROPERTIES
+    
+    var fruits: [Fruit] = fruitsData
+    
+    // MARK: BODY
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(fruits.shuffled()) { item in
+                    FruitRowView(fruit: item)
+                        .padding(.vertical, 4)
+                }
+            }
+            .navigationTitle("Fruits")
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(fruits: fruitsData)
 }
---
+++ Fruits/Fruits/View/FruitRowView.swift
@@ -0,0 +1,44 @@
+//
+//  FruitRowView.swift
+//  Fruits
+//
+//  Created by Aakarshit Jaswal on 26/11/23.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    
+    // MARK: PROPERTIES
+    
+    var fruit: Fruit
+    
+    // MARK: BODY
+    var body: some View {
+        HStack {
+            Image(fruit.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80, alignment: .center)
+                .shadow(color: Color(white: 0, opacity: 0.3), radius: 3, x:3, y:3)
+                .background(
+                    LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom)
+                )
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(fruit.title)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                
+                Text(fruit.headline)
+                    .font(.caption)
+                    .foregroundStyle(Color.secondary)
+            }
+        }//: HSTACK
+    }
+}
+
+#Preview {
+    FruitRowView(fruit: fruitsData[0])
+}
