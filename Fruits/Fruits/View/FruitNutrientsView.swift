5b833106eb3c9ce1d5908eff3745b08756d977f2
Aakarshit Jaswal <70897906+aakarshitjaswal@users.noreply.github.com>
2023-12-05 16:28:28 +0530

    SETUP: FruitNutrientsView

---
+++ Fruits/Fruits/View/FruitNutrientsView.swift
@@ -0,0 +1,23 @@
+//
+//  FruitNutrientsView.swift
+//  Fruits
+//
+//  Created by Aakarshit Jaswal on 05/12/23.
+//
+
+import SwiftUI
+
+struct FruitNutrientsView: View {
+    // MARK: PROPERTIES
+    
+    // MARK: BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitNutrientsView()
+        .preferredColorScheme(.dark)
+        .padding()
+}
